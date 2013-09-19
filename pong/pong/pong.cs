61db1c5827b9ffb86fd223c7d40899c6e4e314d8
tmx7x <tmarttala@gmail.com>
2013-09-19 08:19:25 -0700

    tutorial is dun

--- pong/pong/pong.cs
+++ pong/pong/pong.cs
@@ -15,10 +15,17 @@ public class pong : PhysicsGame
     PhysicsObject maila1;
     PhysicsObject maila2;
 
+    PhysicsObject vasenReuna;
+    PhysicsObject oikeaReuna;
+
+    IntMeter pelaajan1Pisteet;
+    IntMeter pelaajan2Pisteet;
+
     public override void Begin()
     {
         LuoKentta();
         AsetaOhjaimet();
+        LisaaLaskurit();
         AloitaPeli();
     }
 
@@ -29,12 +36,29 @@ public class pong : PhysicsGame
         pallo.X = -200.0;
         pallo.Y = 0.0;
         pallo.Restitution = 1.0;
+        pallo.MomentOfInertia = Double.PositiveInfinity;
         Add(pallo);
+        AddCollisionHandler(pallo, KasittelePallonTormays);
 
         maila1 = LuoMaila(Level.Left + 20.0, 0.0);
         maila2 = LuoMaila(Level.Right - 20.0, 0.0);
 
-        Level.CreateBorders(1.0, false);
+        vasenReuna = Level.CreateLeftBorder();
+        vasenReuna.Restitution = 1.0;
+        vasenReuna.IsVisible = false;
+
+        oikeaReuna = Level.CreateRightBorder();
+        oikeaReuna.Restitution = 1.0;
+        oikeaReuna.IsVisible = false;
+
+        PhysicsObject ylaReuna = Level.CreateTopBorder();
+        ylaReuna.Restitution = 1.0;
+        ylaReuna.IsVisible = false;
+
+        PhysicsObject alaReuna = Level.CreateBottomBorder();
+        alaReuna.Restitution = 1.0;
+        alaReuna.IsVisible = false;
+
         Level.Background.Color = Color.Black;
 
         Camera.ZoomToLevel();
@@ -49,6 +73,41 @@ public class pong : PhysicsGame
         maila.Restitution = 1.0;
         Add(maila);
         return maila;
+    }
+
+    void LisaaLaskurit()
+    {
+        pelaajan1Pisteet = LuoPisteLaskuri(Screen.Left + 100.0, Screen.Top - 100.0);
+        pelaajan2Pisteet = LuoPisteLaskuri(Screen.Right - 100.0, Screen.Top - 100.0);
+    }
+
+    IntMeter LuoPisteLaskuri(double x, double y)
+    {
+        IntMeter laskuri = new IntMeter(0);
+        laskuri.MaxValue = 10;
+
+        Label naytto = new Label();
+        naytto.BindTo(laskuri);
+        naytto.X = x;
+        naytto.Y = y;
+        naytto.TextColor = Color.White;
+        naytto.BorderColor = Level.Background.Color;
+        naytto.Color = Level.Background.Color;
+        Add(naytto);
+
+        return laskuri;
+    }
+
+    void KasittelePallonTormays(PhysicsObject pallo, PhysicsObject kohde)
+    {
+        if (kohde == oikeaReuna)
+        {
+            pelaajan1Pisteet.Value += 1;
+        }
+        else if (kohde == vasenReuna)
+        {
+            pelaajan2Pisteet.Value += 1;
+        }
     }
 
     void AloitaPeli()
@@ -75,14 +134,17 @@ public class pong : PhysicsGame
 
     void AsetaNopeus(PhysicsObject maila, Vector nopeus)
     {
+        if ((nopeus.Y < 0) && (maila.Bottom < Level.Bottom))
+        {
+            maila.Velocity = Vector.Zero;
+            return;
+        }
+        if ((nopeus.Y > 0) && (maila.Top > Level.Top))
+        {
+            maila.Velocity = Vector.Zero;
+            return;
+        }
+
         maila.Velocity = nopeus;
     }
-    void asetanopeus(PhysicsObject maila, Vector nopeus);
-    {
-    
-    if (maila.top > Level.top)
 }
-
-
-
-            
